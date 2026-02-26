49283695148aca8616c07870be6a4aa6513ac889
Vivesh Nidhansing <vivesh13@hotmail.com>
2026-02-26 13:23:04 +0100

    test
    idk

--- Quest.cs
+++ Quest.cs
@@ -1,4 +1,11 @@
-public class Quest
+class Quest
 {
+    public Quest(int id, string name, string Description)
+    {
+        this.id = id;
+        this.name = name;
+        this.Description = Description;
+    }
+
 
 }
