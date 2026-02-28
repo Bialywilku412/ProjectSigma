d00cc64a2a79e55accba61ba14134b863e67999c
Bialywilku412 <maciekplewka0702@gmail.com>
2026-02-28 08:53:47 +0100

    Player class with basic needed methods (Attack, IsAlive, MoveTo, TakeDamage)

--- Project Alpha/Player.cs
+++ Project Alpha/Player.cs
@@ -1,3 +1,31 @@
 ﻿internal class Player
 {
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+    public Weapon CurrentWeapon;
+    public Location CurrentLocation;
+
+    public Player(string name, Weapon currentWeapon, Location currentLocation)
+    {
+        Name = name;
+        MaximumHitPoints = 100;
+        CurrentHitPoints = 100;
+        CurrentWeapon = currentWeapon;
+        CurrentLocation = currentLocation;
+    }
+
+    public int Attack() => World.RandomGenerator.Next(1, CurrentWeapon.MaximumDamage + 1);
+    public bool IsAlive() => CurrentHitPoints > 0;
+    public void MoveTo(Location newLocation) => CurrentLocation = newLocation;
+    public void TakeDamage(int damage)
+    {
+        CurrentHitPoints -= damage;
+
+        if (CurrentHitPoints < damage)
+        {
+            CurrentHitPoints = 0;
+        }
+    }
+
 }
