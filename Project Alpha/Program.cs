0c473013172e8785b23a2cbd9d245093cabad1f8
Bialywilku412 <maciekplewka0702@gmail.com>
2026-02-28 09:03:39 +0100

    Entering hero name and creating player in program

--- Project Alpha/Program.cs
+++ Project Alpha/Program.cs
@@ -3,5 +3,15 @@
     static void Main()
     {
         Console.WriteLine("Deafault setup");
+
+        Console.WriteLine("Enter Hero's name");
+        string playerName = Console.ReadLine();
+        // Weapon and location has to be created yet
+        Player player = new Player(playerName, "rusty sword", "home");
+        while (true)
+        {
+
+        }
+
     }
 }
