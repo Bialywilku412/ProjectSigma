2abeb9fffa793eef9820578dad64c3ed2551445c
Daniel <daniel_stef@abv.bg>
2026-03-06 15:13:00 +0100

    Revert "Merge branch 'main' into Monsters"
    This reverts commit d9fae0f814a55a9ed3e6abc33a7b296369d455bc, reversing
    changes made to e35f3a98ae66230d849dbdb2c1bd724c4f33c291.

--- Project Alpha/Player.cs
+++ Project Alpha/Player.cs
@@ -1,72 +1,31 @@
-﻿using System.Net;
-
-internal class Player
+﻿internal class Player
 {
     public string Name;
     public int CurrentHitPoints;
     public int MaximumHitPoints;
     public Weapon CurrentWeapon;
     public Location CurrentLocation;
-    public Quest CurrentQuest;
 
-    public Player(string name, Weapon currentWeapon, Location currentLocation, Quest currentQuest)
+    public Player(string name, Weapon currentWeapon, Location currentLocation)
     {
         Name = name;
         MaximumHitPoints = 100;
         CurrentHitPoints = 100;
         CurrentWeapon = currentWeapon;
         CurrentLocation = currentLocation;
-        CurrentQuest = currentQuest;
     }
 
     public int Attack() => World.RandomGenerator.Next(1, CurrentWeapon.MaximumDamage + 1);
 
     public bool IsAlive() => CurrentHitPoints > 0;
 
-    public void MoveTo(Location newLocation)
-    {
-        this.CurrentLocation = newLocation;
-
-        if (CurrentLocation.QuestAvailableHere != null)
-        {
-            if (CurrentQuest == null || CurrentQuest.ID != CurrentLocation.QuestAvailableHere.ID)
-            {
-                Console.WriteLine($"\nYou arived at {newLocation.Name}.");
-                Console.WriteLine($"There is a Quest available {CurrentLocation.QuestAvailableHere.Name}");
-                Console.WriteLine("Do you Acept this Quest? (yes/no)");
-                string choice = Console.ReadLine().ToLower();
-
-                if (choice == "yes")
-                {
-                    this.CurrentQuest = CurrentLocation.QuestAvailableHere;
-                    Console.WriteLine($"Your quest journey begins now. {this.CurrentQuest.Name}");
-                }
-                else if (choice == "no")
-                {
-                    Console.WriteLine("You are about to abort the Quest are you really not man enough to take it?");
-                    Console.WriteLine("yes / no");
-                    string choice_2 = Console.ReadLine().ToLower();
-
-                    if (choice_2 == "yes" || choice_2 == "no")
-                    {
-                        this.CurrentQuest = CurrentLocation.QuestAvailableHere;
-                        Console.WriteLine($"Thats what we like to see you Quest start now. {this.CurrentQuest.Name}");                       
-                    }
-                    else
-                    {
-                        Console.WriteLine("You canceled the Quest chicken");
-                    }
-                }
-                
-            }
-        }
-    }
+    public void MoveTo(Location newLocation) => CurrentLocation = newLocation;
 
     public void TakeDamage(int damage)
     {
         CurrentHitPoints -= damage;
 
-        if (CurrentHitPoints < 0)
+        if (CurrentHitPoints < damage)
         {
             CurrentHitPoints = 0;
         }
@@ -79,7 +38,7 @@ internal class Player
             case "south":
                 if (CurrentLocation.LocationToSouth != null)
                 {
-                    MoveTo(CurrentLocation.LocationToSouth);
+                    CurrentLocation = CurrentLocation.LocationToSouth;
                     return true;
                 }
                 else
@@ -89,7 +48,7 @@ internal class Player
             case "north":
                 if (CurrentLocation.LocationToNorth != null)
                 {
-                    MoveTo(CurrentLocation.LocationToNorth);
+                    CurrentLocation = CurrentLocation.LocationToNorth;
                     return true;
                 }
                 else
@@ -99,7 +58,7 @@ internal class Player
             case "west":
                 if (CurrentLocation.LocationToWest != null)
                 {
-                    MoveTo(CurrentLocation.LocationToWest);
+                    CurrentLocation = CurrentLocation.LocationToWest;
                     return true;
                 }
                 else
@@ -109,7 +68,7 @@ internal class Player
             case "east":
                 if (CurrentLocation.LocationToEast != null)
                 {
-                    MoveTo(CurrentLocation.LocationToEast);
+                    CurrentLocation = CurrentLocation.LocationToEast;
                     return true;
                 }
                 else
--- Project Alpha/Weapon.cs
+++ Project Alpha/Weapon.cs
@@ -11,5 +11,9 @@
         MaximumDamage = maximumdamage;
     }
 
-    public static List<Weapon> Weapons = new List<Weapon>();
+    public static List<Weapon> Weapons = new List<Weapon>()
+    {
+        new Weapon(1, "Rusty Sword", 2)
+
+    };
 }
