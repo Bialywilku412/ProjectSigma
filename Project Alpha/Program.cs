35914e708ca5973b31af13459fd7b4812d8f8ef8
Andrei-ierdnA <an.plchkv@gmail.com>
2026-03-10 14:48:13 +0100

    Fix for SeeGameStats function, and removed comments form outdated code

--- Project Alpha/Program.cs
+++ Project Alpha/Program.cs
@@ -8,15 +8,6 @@
 
         Player player = new Player(playerName, World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD), World.LocationByID(1), null);
 
-        // this change is purely for aestheticall reasons, leaving this commented out if you guys prefer this way
-        /*
-        Console.WriteLine($"Your name is {player.Name}. You live in a town called Riverbend");
-        Console.WriteLine("You always dreamed to be a hero. And now when you heard that your town is being terrorized by big spiders.");
-        Console.WriteLine("You decided to do all you can yo help your town");
-        Console.WriteLine("In chest in your home there is a rusty sword that once your father fought with");
-        Console.WriteLine("You take it and from now on you swear to protect people of your town");
-        */
-
         Console.WriteLine(
             $"Your name is {player.Name}. You live in a town called Riverbend\n" +
             "You always dreamed to be a hero. And now when you heard that your town is being terrorized by big spiders.\n" +
@@ -111,7 +102,9 @@
     {
         Console.WriteLine($"{player.Name}: {player.CurrentHitPoints}/{player.MaximumHitPoints}");
         Console.WriteLine($"Current weapon: {player.CurrentWeapon.Name} Damage:1-{player.CurrentWeapon.MaximumDamage}");
-        Console.WriteLine($"Current Quest:{player.CurrentQuest.Name}");
+
+        // if the player has a quest, 
+        if (player.CurrentQuest != null) { Console.WriteLine($"Current Quest:{player.CurrentQuest.Name}"); }
     }
 
 
@@ -184,11 +177,4 @@
             p.CurrentQuest = null;
         }
     }
-
-    //static void Move(Player player)
-    //{
-    //    Console.WriteLine("Where would you like to go?");
-    //    Console.WriteLine($"You are at: {player.CurrentLocation}. From here you can go:");
-    //    // Location functions has to be implemented yet
-    //}
 }
